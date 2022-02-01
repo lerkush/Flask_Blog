c3cfcc8e62082da603ca66ab1d123bfb5e926a79
Lütfiye ERKUŞ <lutfiyye@gmail.com>
2022-02-02 01:39:01 +0300

    It is created by using Flask

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = 'b723ff6d7896ab33008935cffcd0f60f'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('EMAIL_USER')
+    MAIL_PASSWORD = os.getenv('EMAIL_PASS')
